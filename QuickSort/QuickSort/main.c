309383ad9cc474972e8cc94d054ffed777194953
chan <videoal@naver.com>
2016-11-25 23:20:22 +0900

    quicksort 구현

--- QuickSort/QuickSort/main.c
+++ QuickSort/QuickSort/main.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
 
 //
+void quickSort(int a[], int l, int r)
+{
+	if (l < r) {
+		int pivot, s, e;
+		int tmp;
+
+		// partitioning
+		pivot = a[l];
+		s = l + 1;
+		e = r;
+
+		while (s <= e) {
+			while (s <= r && a[s] <= pivot) s++;
+			while (e >= l + 1 && a[e] >= pivot) e--;
+
+			if (s < e) {
+				tmp = a[s]; a[s] = a[e]; a[e] = tmp;
+			}
+		}
+
+		tmp = a[e]; a[e] = pivot; a[l] = tmp;
+
+
+		quickSort(a, l, e - 1);
+		quickSort(a, e + 1, r);
+	}
+}
 
 void main(void)
 {
@@ -13,6 +40,8 @@ void main(void)
 		printf(",%d", a[i]);
 	}
 	printf("}\n");
+
+	quickSort(a, 0, 4);
 
 
 	printf("sorted value:{");
